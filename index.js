43a32b699ec2f2d20a32958a6f19964cb1a09204
Husain Baghwala <husainbw123@gmail.com>
2025-09-25 19:36:22 +0530

    fix: allow all CORS headers instead of restricting to Content-Type and Authorization

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(
   cors({
     origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["*"],
     credentials: true,
   })
 );
